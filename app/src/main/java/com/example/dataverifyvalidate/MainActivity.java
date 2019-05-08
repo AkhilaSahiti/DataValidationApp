cdb50d42b437e52ecdccd56383c8f7afeeb6b89c
AkhilaSahiti <racherlasahiti@gmail.com>
2019-05-08 12:24:21 +0530

    checkDataEntered()

--- app/src/main/java/com/example/dataverifyvalidate/MainActivity.java
+++ app/src/main/java/com/example/dataverifyvalidate/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dataverifyvalidate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -10,14 +11,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String vname, vdept, vrole, vsal;
 
+    private String vname, vdept, vrole, vsal;
+    Button ondisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ondisplay = (Button) findViewById(R.id.display);
+       // ondisplay.setOnClickListener(new View.OnClickListener() {
+            //@Override
+                // });
     }
-    public void onDisplay(View view){
+
+    public void onDisplay(View view) {
         TextView name = (TextView) findViewById(R.id.name);
         EditText enter_name = (EditText) findViewById(R.id.enter_name);
 
@@ -36,30 +44,31 @@ public class MainActivity extends AppCompatActivity {
         vdept = department.getText().toString().trim();
         vrole = role.getText().toString().trim();
         vsal = sal.getText().toString().trim();
+
+        checkDataEntered();
     }
 
-        public boolean validation() {
-            boolean valid = true;
+    void checkDataEntered(){
             if (vname.isEmpty() || vname.length() < 3) {
                 Toast toast = Toast.makeText(this, "Name should contain atleast 3 letters!", Toast.LENGTH_SHORT);
                 toast.show();
-                valid = false;
+               // valid = false;
             }
             if (vdept.equalsIgnoreCase("Choose any")) {
                 Toast toast = Toast.makeText(this, "Can`t be empty!", Toast.LENGTH_SHORT);
                 toast.show();
-                valid = false;
+                //valid = false;
             }
             if (vrole.equalsIgnoreCase("Choose any")) {
                 Toast toast = Toast.makeText(this, "Cannot be empty!", Toast.LENGTH_SHORT);
                 toast.show();
-                valid = false;
+                //valid = false;
             }
             if (Integer.valueOf(vsal) < 20000 || Integer.valueOf(vsal) > 25000) {
                 Toast toast = Toast.makeText(this, "Enter valid amount!", Toast.LENGTH_SHORT);
                 toast.show();
-                valid = false;
+                //valid = false;
             }
-            return valid;
+            //return valid;
         }
 }
