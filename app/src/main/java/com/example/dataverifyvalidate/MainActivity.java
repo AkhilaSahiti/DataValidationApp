1df5d048df44d863b499d3a8a2f59234ef2ff65e
AkhilaSahiti <racherlasahiti@gmail.com>
2019-05-08 11:16:40 +0530

    data verification and validation

---
+++ app/src/main/java/com/example/dataverifyvalidate/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.dataverifyvalidate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String vname, vdept, vrole, vsal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onDisplay(View view){
+        TextView name = (TextView) findViewById(R.id.name);
+        EditText enter_name = (EditText) findViewById(R.id.enter_name);
+
+        TextView department = (TextView) findViewById(R.id.department);
+        Spinner dept = (Spinner) findViewById(R.id.dept);
+        String deptType = String.valueOf(dept.getSelectedItem());
+
+        TextView role = (TextView) findViewById(R.id.role);
+        Spinner roles = (Spinner) findViewById(R.id.roles);
+        String roleType = String.valueOf(roles.getSelectedItem());
+
+        TextView salary = (TextView) findViewById(R.id.salary);
+        EditText sal = (EditText) findViewById(R.id.sal);
+
+        vname = enter_name.getText().toString().trim();
+        vdept = department.getText().toString().trim();
+        vrole = role.getText().toString().trim();
+        vsal = sal.getText().toString().trim();
+    }
+
+        public boolean validation() {
+            boolean valid = true;
+            if (vname.isEmpty() || vname.length() < 3) {
+                Toast toast = Toast.makeText(this, "Name should contain atleast 3 letters!", Toast.LENGTH_SHORT);
+                toast.show();
+                valid = false;
+            }
+            if (vdept.equalsIgnoreCase("Choose any")) {
+                Toast toast = Toast.makeText(this, "Can`t be empty!", Toast.LENGTH_SHORT);
+                toast.show();
+                valid = false;
+            }
+            if (vrole.equalsIgnoreCase("Choose any")) {
+                Toast toast = Toast.makeText(this, "Cannot be empty!", Toast.LENGTH_SHORT);
+                toast.show();
+                valid = false;
+            }
+            if (Integer.valueOf(vsal) < 20000 || Integer.valueOf(vsal) > 25000) {
+                Toast toast = Toast.makeText(this, "Enter valid amount!", Toast.LENGTH_SHORT);
+                toast.show();
+                valid = false;
+            }
+            return valid;
+        }
+}
