045c24ec747fe46ef8a601e4323c7b2c70de71b7
AkhilaSahiti <racherlasahiti@gmail.com>
2019-05-10 16:15:13 +0530

    email formation

--- app/src/main/java/com/example/dataverifyvalidate/validationsuccess.java
+++ app/src/main/java/com/example/dataverifyvalidate/validationsuccess.java
@@ -1,9 +1,13 @@
 package com.example.dataverifyvalidate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Currency;
@@ -36,5 +40,31 @@ public class validationsuccess extends AppCompatActivity {
         TextView role = (TextView) findViewById(R.id.personrole);
         role.setText(urrole);
 
+        Button email = (Button) findViewById(R.id.email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendemail();
+            }
+        });
     }
+    protected void sendemail(){
+
+        Intent eintent = new Intent(Intent.ACTION_VIEW);
+
+        eintent.setData(Uri.parse("mailto:"));
+        eintent.setType("text/plain");
+        eintent.putExtra(Intent.EXTRA_SUBJECT,"subject");
+        eintent.putExtra(Intent.EXTRA_TEXT,"message");
+
+
+        try {
+            startActivity(eintent.createChooser(eintent, "Send mail..."));
+            finish();
+        }catch (android.content.ActivityNotFoundException  ex){
+            Toast.makeText(validationsuccess.this, "No email client installed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
